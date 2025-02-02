b9e1d9caf0b4690373a99d2d050038aff7f49905
arpit <arpit2003y@gmail.com>
2025-02-02 18:34:51 +0530

    fix(db):db_model_working

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,19 +1,21 @@
 import { env } from "~/env";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle, NeonClient } from "drizzle-orm/neon-serverless";
 import * as schema from "./schema";
-import { neon, NeonQueryFunction } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  client: NeonQueryFunction<false, false> | undefined;
+  client: NeonClient | undefined;
 };
 
-const sql = neon(env.DATABASE_URL);
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+});
 
-export const client = globalForDb.client ?? sql;
+export const client = globalForDb.client ?? pool;
 if (env.NODE_ENV !== "production") globalForDb.client = client;
 
 export const db = drizzle(client, { schema });
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,13 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { serial, text, pgTableCreator, integer } from "drizzle-orm/pg-core";
+import {
+  serial,
+  text,
+  pgTableCreator,
+  integer,
+  index,
+} from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -11,8 +17,28 @@ import { serial, text, pgTableCreator, integer } from "drizzle-orm/pg-core";
  */
 export const createTable = pgTableCreator((name) => `drive_${name}`);
 
-export const users = createTable("users_table", {
-  id: serial("id").primaryKey(),
-  name: text("name").notNull(),
-  age: integer("age").notNull(),
-});
+export const files = createTable(
+  "files_tables",
+  {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    size: integer("size").notNull(),
+    url: text("url").notNull(),
+    parent: integer("parent").notNull(),
+  },
+  (t) => ({
+    indexes: [index("parent_index").on(t.parent)],
+  }),
+);
+
+export const folders = createTable(
+  "folders_table",
+  {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    parent: integer("parent"),
+  },
+  (t) => ({
+    indexes: [index("parent_index").on(t.parent)],
+  }),
+);
